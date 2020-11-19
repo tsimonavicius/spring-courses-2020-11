28a016bb9d359857715af0f7208eb8665da19bbe
Tadas Simonavičius <tadas@inventi.lt>
2020-11-19 20:29:20 +0200

    make hibernate entity

--- src/main/java/com/codeacademy/eshop/product/model/Product.java
+++ src/main/java/com/codeacademy/eshop/product/model/Product.java
@@ -3,6 +3,7 @@ package com.codeacademy.eshop.product.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
@@ -10,15 +11,23 @@ import java.math.BigDecimal;
  */
 @Getter
 @Setter
+@Entity
+@Table(name = "Products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "product_name")
     private String name;
 
+    @Column(name = "in_stock")
     private Integer inStock;
 
+    @Column(name = "price")
     private BigDecimal price;
 
+    @Column(name = "description")
     private String description;
 }
