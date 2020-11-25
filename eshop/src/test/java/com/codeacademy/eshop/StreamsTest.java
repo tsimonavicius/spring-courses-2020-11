533e841824365448ab82a70f38b302291ec2f167
Tadas Simonavičius <tadas@inventi.lt>
2020-11-25 20:18:01 +0200

    Find most expensive Samsung (for vs Streams)

--- eshop/src/test/java/com/codeacademy/eshop/StreamsTest.java
+++ eshop/src/test/java/com/codeacademy/eshop/StreamsTest.java
@@ -1,12 +1,17 @@
 package com.codeacademy.eshop;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
 
 public class StreamsTest {
 
@@ -38,25 +43,64 @@ public class StreamsTest {
     @Test
     public void testStreams() {
         List<MobilePhone> phones = List.of(
-                new MobilePhone("Iphone 12", 1000.50),
-                new MobilePhone("Samsung S10", 300.99),
-                new MobilePhone("Samsung S20", 999.99));
+                new MobilePhone("Iphone", "12 Pro", 1000.50),
+                new MobilePhone("Samsung", "S10", 300.99),
+                new MobilePhone("Samsung", "Note 10", 899.90),
+                new MobilePhone("Samsung", "S20+", 1200.99),
+                new MobilePhone("Samsung", "S20", 999.99));
 
-        BigDecimal sum = BigDecimal.ZERO;
+//        BigDecimal sum = BigDecimal.ZERO;
+//
+//        for (MobilePhone phone : phones) {
+////            BigDecimal price = phone.getPrice();
+////            sum = addPrice(sum, price);
+//        }
+//
+//        System.out.println("For loop: " + sum);
+//
+//        System.out.println("Streams: " + phones.stream() // source - phone
+////                .map(mobilePhone -> mobilePhone.getPrice())
+//                .mapToDouble(MobilePhone::getPrice) // intermediate operation 0-...
+//                .sum());
+////                .reduce(BigDecimal.ZERO, (initialValue, price) -> initialValue.add(price))); // terminal operation
+//
+//        List<String> aaa = new ArrayList<>();
 
+        findSamsungWithFor(phones);
+        findSamsungWithStreams(phones);
+    }
+
+    // Surasti brangiausia samsung telefona ir atvaizduoti jo modeli
+    private void findSamsungWithFor(List<MobilePhone> phones) {
+
+        String brangiausiasSamsungModelis = null;
+        double brangiausioSamsungModelioKaina = 0;
         for (MobilePhone phone : phones) {
-//            BigDecimal price = phone.getPrice();
-//            sum = addPrice(sum, price);
+            if (phone.getBrand().equals("Samsung") && brangiausioSamsungModelioKaina < phone.getPrice()) {
+                brangiausioSamsungModelioKaina = phone.getPrice();
+                brangiausiasSamsungModelis = phone.getModel();
+            }
         }
 
-        System.out.println("For loop: " + sum);
+        System.out.println(brangiausiasSamsungModelis);
 
-        System.out.println("Streams: " + phones.stream() // source - phone
-                .mapToDouble(MobilePhone::getPrice) // intermediate operation 0-...
-                .sum());
-//                .reduce(BigDecimal.ZERO, (initialValue, price) -> initialValue.add(price))); // terminal operation
+    }
 
-        List<String> aaa = new ArrayList<>();
+    private void findSamsungWithStreams(List<MobilePhone> phones) {
+
+        String brangiausiasSamsungas = phones.stream()
+                .filter(MobilePhone::isSamsung)
+                .sorted(comparing(MobilePhone::getPrice).reversed())
+                .findFirst()
+                .get()
+                .getModel();
+
+        System.out.println(brangiausiasSamsungas);
+
+    }
+
+    private static boolean suraskSamsunga(MobilePhone phone) {
+        return phone.getBrand().equals("Samsung");
     }
 
     private <T> BigDecimal extractFromObjectAndLog(T objektas, Funkcija<T> funkcija) {
@@ -88,14 +132,19 @@ public class StreamsTest {
 //        }
 //    }
 
+    public static double kazkoksMetodas(MobilePhone mobilePhone) {
+        return mobilePhone.getPrice();
+    }
+
     @Getter
+    @AllArgsConstructor
     private static class MobilePhone {
         private String brand;
+        private String model;
         private double price;
 
-        public MobilePhone(String brand, double price) {
-            this.brand = brand;
-            this.price = price;
+        public boolean isSamsung() {
+            return brand.equals("Samsung");
         }
     }
 }
