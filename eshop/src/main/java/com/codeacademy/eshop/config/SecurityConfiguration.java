309ae655253cadfd9bbc0126939458bfeab63b1a
Tadas Simonavičius <tadas@inventi.lt>
2020-12-09 21:19:14 +0200

    Added inmemory user storage

--- eshop/src/main/java/com/codeacademy/eshop/config/SecurityConfiguration.java
+++ eshop/src/main/java/com/codeacademy/eshop/config/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.codeacademy.eshop.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -24,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .loginProcessingUrl("/prisijungimas")
                     .usernameParameter("user")
                     .passwordParameter("pass")
-                    .defaultSuccessUrl("/user")
+                    .defaultSuccessUrl("/private/user")
                     .failureUrl("/prisijungimas?error")
                     .and()
                 .logout()
@@ -37,5 +41,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity webSecurity) {
         webSecurity.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{bcrypt}$2a$10$IgO.t36FA2.M15EMK2l7EuN3cdiRRvYOHmhQqH8ELnCjf7FUhZiy.")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder().encode("admin"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
