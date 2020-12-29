4b994547b0b44419978de680a3d2db7d4abe0b1b
Tadas Simonavičius <tadas@inventi.lt>
2020-12-29 22:10:39 +0200

    Created REST endpoint to get product by ID

--- eshop/src/main/java/com/codeacademy/eshop/product/controller/ProductsRestController.java
+++ eshop/src/main/java/com/codeacademy/eshop/product/controller/ProductsRestController.java
@@ -5,13 +5,14 @@ import com.codeacademy.eshop.product.service.ProductService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/public/product")
+@RequestMapping("/public/api/product")
 public class ProductsRestController {
 
     private final ProductService productService;
@@ -20,13 +21,18 @@ public class ProductsRestController {
         this.productService = productService;
     }
 
-    @GetMapping(value = "/rest/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getAllProductsAsJson() {
         return productService.getAllProducts(PageRequest.of(0, 10)).getContent();
     }
 
-    @GetMapping(value = "/rest/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    @GetMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
     public List<Product> getAllProductsAsXml() {
         return productService.getAllProducts(PageRequest.of(0, 10)).getContent();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Product getProduct(@PathVariable long id) {
+        return productService.getProductById(id);
     }
 }
