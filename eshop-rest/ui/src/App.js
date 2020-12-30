df482afb4c2fc364f4e4ae1670740f1a64f4d125
Tadas Simonavičius <tadas@inventi.lt>
2020-12-30 21:56:34 +0200

    Created class component with state

--- eshop-rest/ui/src/App.js
+++ eshop-rest/ui/src/App.js
@@ -1,11 +1,13 @@
 import MyAwesomeComponent from "./components/MyAwesomeComponent";
 import Counter from "./components/Counter";
+import HelloWorld from "./components/HelloWorld";
 
 function App() {
   return (
     <div className="App">
         <MyAwesomeComponent />
         <Counter tekstas="Niekas nepaspausta" />
+        <HelloWorld/>
     </div>
   );
 }
