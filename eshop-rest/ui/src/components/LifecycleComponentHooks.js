adb999f52653bb7f17bcddf12ab846c6aa4887da
Elvis <elvinas@elvisolutions.com>
2021-01-04 21:43:16 +0200

    UseEffect example

--- eshop-rest/ui/src/components/LifecycleComponentHooks.js
+++ eshop-rest/ui/src/components/LifecycleComponentHooks.js
@@ -1,15 +1,31 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
 export default ({text}) => {
 
+    const [isLoading, setLoader] = useState(true);
+    const [extraText, setExtraText] = useState('');
+
     useEffect(() => {
         console.log("mounted....")
+
+        setTimeout(() => {
+            setLoader(false);
+        }, 2000)
+
+        if (text === 'ok') {
+            setExtraText("!!!! JEEEE !!! Jusu ivestas teksto turinys mums labai patinka!!!")
+        }
+
         return () => {
             console.log("updating or unmounting????????")
         }
     })
 
     return (
-        <div>Viskas ciki piki! Perduotas tekstas su functional component: { text }</div>
+        <div>
+            {isLoading && <span>Kraunasi...</span>}
+            Viskas ciki piki! Perduotas tekstas su functional component: { text }
+            {extraText.length > 0 && <span>{extraText}</span>}
+        </div>
     );
 }
