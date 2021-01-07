f7b6462295a1563c42d73d50ee40e2b6aee70056
Tadas Simonavičius <tadas@inventi.lt>
2021-01-07 21:57:54 +0200

    Added loading spinner

--- eshop-rest/ui/src/pages/Products/Products.js
+++ eshop-rest/ui/src/pages/Products/Products.js
@@ -4,18 +4,28 @@ import {fetchProducts} from "../../api/productsApi";
 
 export default () => {
 	const [products, setProducts] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
-		fetchProducts().then(response => {
+		setTimeout(() => fetchProducts().then(response => {
 			setProducts(response.data)
+			setIsLoading(false)
 			console.log('response', response)
-		})
+		}), 2000)
+
 	}, [])
 
 	return (
 		<>
 			<h1>Products Page!</h1>
-			<h2>{JSON.stringify(products)}</h2>
+			{
+				isLoading ?
+					(
+						<div className="spinner-border" role="status">
+						</div>
+					) :
+					<h2>{JSON.stringify(products)}</h2>
+			}
 			<Link to="/products/new">
 				<button type="button" className="btn btn-primary">Sukurti produkta</button>
 			</Link>
