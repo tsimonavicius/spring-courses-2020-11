df88fa932eb11a9ba277549d21e3253589318f6f
Tadas Simonavičius <tadas@inventi.lt>
2021-01-07 20:01:07 +0200

    Added navigation to Product form

--- eshop-rest/ui/src/components/Content/Content.js
+++ eshop-rest/ui/src/components/Content/Content.js
@@ -2,12 +2,16 @@ import {Switch, Route} from "react-router-dom"
 import LandingPage from "../../pages/LandingPage/LandingPage";
 import Products from "../../pages/Products/Products";
 import About from "../../pages/About/About";
+import ProductForm from "../../pages/ProductForm/ProductForm";
 
 export default () => (
 	<main className="container">
 		<Switch>
 			<Route exact path="/">
 				<LandingPage/>
+			</Route>
+			<Route path="/products/new">
+				<ProductForm/>
 			</Route>
 			<Route path="/products">
 				<Products/>
--- eshop-rest/ui/src/pages/Products/Products.js
+++ eshop-rest/ui/src/pages/Products/Products.js
@@ -1,3 +1,10 @@
+import {Link} from "react-router-dom";
+
 export default () => (
-	<h1>Products Page!</h1>
+	<>
+		<h1>Products Page!</h1>
+		<Link to="/products/new">
+			<button type="button" className="btn btn-primary">Sukurti produkta</button>
+		</Link>
+	</>
 )
