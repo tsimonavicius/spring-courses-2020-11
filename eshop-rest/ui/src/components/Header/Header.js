63be0e44237b51618c1ca3633a8369f6df63895d
Tadas Simonavičius <tadas@inventi.lt>
2021-01-07 19:44:24 +0200

    Changed to NavLinks to show current page

--- eshop-rest/ui/src/components/Header/Header.js
+++ eshop-rest/ui/src/components/Header/Header.js
@@ -1,17 +1,17 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default () => (
 	<header>
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<div className="container justify-content-start">
-				<Link className="navbar-brand" to="/">E-shop</Link>
+				<NavLink className="navbar-brand" exact to="/">E-shop</NavLink>
 
 				<ul className="navbar-nav">
 					<li className="nav-item">
-						<Link className="nav-link" to="/products">Produktai</Link>
+						<NavLink className="nav-link" to="/products">Produktai</NavLink>
 					</li>
 					<li className="nav-item">
-						<Link className="nav-link" to="/about">Apie sistema</Link>
+						<NavLink className="nav-link" to="/about">Apie sistema</NavLink>
 					</li>
 				</ul>
 			</div>
