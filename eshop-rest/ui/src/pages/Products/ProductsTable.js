5d839d0a65dd14fac60061947b953ad359ccc028
Elvis <elvinas@elvisolutions.com>
2021-01-12 21:51:54 +0200

    Adds material-ui table in productsTable component

--- eshop-rest/ui/src/pages/Products/ProductsTable.js
+++ eshop-rest/ui/src/pages/Products/ProductsTable.js
@@ -1,38 +1,53 @@
 import PropTypes from 'prop-types';
+import Button from "@material-ui/core/Button";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import React from "react";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import TableContainer from "@material-ui/core/TableContainer";
 
 function ProductTable(props) {
 
     const { products, handleDeleteClick } = props;
 
     return (
-        <table>
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Product name</th>
-                <th>Description</th>
-                <th>In Stock</th>
-                <th>Price</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-                products.map(p => (
-                    <tr key={p.id}>
-                        <td>{p.id}</td>
-                        <td>{p.name}</td>
-                        <td>{p.description}</td>
-                        <td>{p.inStock}</td>
-                        <td>{p.price}</td>
-                        <th>
-                            <button onClick={() => handleDeleteClick(p.id)}>Delete product</button>
-                        </th>
-                    </tr>
-                ))
-            }
-            </tbody>
-        </table>
+        <TableContainer>
+            <Table >
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="center">#</TableCell>
+                        <TableCell align="center">Product name</TableCell>
+                        <TableCell align="center">Description</TableCell>
+                        <TableCell align="center">In Stock</TableCell>
+                        <TableCell align="center">Price</TableCell>
+                        <TableCell align="center"/>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        products.map(p => (
+                            <TableRow key={p.id}>
+                                <TableCell>{p.id}</TableCell>
+                                <TableCell>{p.name}</TableCell>
+                                <TableCell>{p.description}</TableCell>
+                                <TableCell>{p.inStock}</TableCell>
+                                <TableCell>{p.price}</TableCell>
+                                <TableCell>
+                                    <Button
+                                        variant="outlined"
+                                        color="secondary"
+                                        size={"small"}
+                                        onClick={() => handleDeleteClick(p.id)}
+                                    >Delete</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
