d2184b1f7c89cc57b6f8fbecffd4bae3a8a98bf5
Tadas Simonavičius <tadas@inventi.lt>
2021-01-13 19:56:37 +0200

    Switched to H2 and added initial products

---
+++ eshop-rest/backend/src/main/resources/db/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO PRODUCT (name, in_stock, price, description) VALUES
+('Kremukas0', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas1', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas2', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas3', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas4', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas5', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas6', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas7', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas8', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas9', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas10', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas11', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas12', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas13', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas14', 20, 19.99, 'Kremukas odai sausint'),
+('Kremukas plaukam', 22, 24.99, 'Kremukas plaukams sviestint'),
+('Plauku balzamas', 30, 4.99, 'Balzamas plaukams');
