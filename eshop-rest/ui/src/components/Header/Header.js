28bef76695b5a3e83d0137640a7dd0e785e32b72
Tadas Simonavičius <tadas@inventi.lt>
2021-01-13 18:47:18 +0200

    Added NavBar from Material UI

--- eshop-rest/ui/src/components/Header/Header.js
+++ eshop-rest/ui/src/components/Header/Header.js
@@ -1,20 +1,68 @@
+import {AppBar, CssBaseline, makeStyles, Toolbar, Typography} from "@material-ui/core";
 import {NavLink} from "react-router-dom";
 
-export default () => (
-	<header>
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container justify-content-start">
-				<NavLink className="navbar-brand" exact to="/">E-shop</NavLink>
+const useStyles = makeStyles((theme) => ({
+	'@global': {
+		ul: {
+			margin: 0,
+			padding: 0,
+			listStyle: 'none',
+		},
+	},
+	appBar: {
+		borderBottom: `1px solid ${theme.palette.divider}`,
+	},
+	toolbar: {
+		flexWrap: 'wrap',
+	},
+	toolbarTitle: {
+		flexGrow: 1,
+	},
+	link: {
+		margin: theme.spacing(1, 1.5)
+	},
+	heroContent: {
+		padding: theme.spacing(8, 0, 6),
+	},
+	cardHeader: {
+		backgroundColor:
+			theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+	},
+	cardPricing: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'baseline',
+		marginBottom: theme.spacing(2),
+	},
+	footer: {
+		borderTop: `1px solid ${theme.palette.divider}`,
+		marginTop: theme.spacing(8),
+		paddingTop: theme.spacing(3),
+		paddingBottom: theme.spacing(3),
+		[theme.breakpoints.up('sm')]: {
+			paddingTop: theme.spacing(6),
+			paddingBottom: theme.spacing(6),
+		},
+	},
+}));
 
-				<ul className="navbar-nav">
-					<li className="nav-item">
-						<NavLink className="nav-link" to="/products">Produktai</NavLink>
-					</li>
-					<li className="nav-item">
-						<NavLink className="nav-link" to="/about">Apie sistema</NavLink>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	</header>
-)
+export default () => {
+	const classes = useStyles()
+
+	return (
+		<>
+			<CssBaseline/>
+			<AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
+					<Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+						Eshop
+					</Typography>
+					<nav>
+						<NavLink className={classes.link} to="/products">Produktai</NavLink>
+						<NavLink className={classes.link} to="/about">Apie sistema</NavLink>
+					</nav>
+				</Toolbar>
+			</AppBar>
+		</>
+	)
+}
