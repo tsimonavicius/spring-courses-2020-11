906046e5eb7e21b5179dcef47bb270ed5ed03baf
Elvis <elvinas@elvisolutions.com>
2021-01-18 21:46:36 +0200

    Add external api fetch example, axios concurrent requests, history hook

--- eshop-rest/ui/src/pages/Products/ProductCard.js
+++ eshop-rest/ui/src/pages/Products/ProductCard.js
@@ -1,5 +1,6 @@
-import PropTypes from "prop-types";
 import React from 'react';
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,7 +8,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-function ProductCard({ product }) {
+function ProductCard({ product, text }) {
+
+    const history = useHistory();
+
+    const handleGoBack = () => {
+        history.push("/products")
+    }
 
     const useStyles = makeStyles({
         root: {
@@ -21,25 +28,36 @@ function ProductCard({ product }) {
             fontSize: 14,
             textAlign: "center"
         },
+        textCenter: {
+            textAlign: "center"
+        },
+        m1: {
+            margin: 15,
+        }
     });
 
     const classes = useStyles();
 
     return (
-
-        <Card className={classes.root} variant="outlined">
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    {product.name}
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {product.description}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Edit description</Button>
-            </CardActions>
-        </Card>
+        <>
+            <Button className={classes.m1} onClick={() => handleGoBack()} >Back to products</Button>
+            <Card className={classes.root} variant="outlined">
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography className={classes.textCenter} variant="body2" component="p">
+                        {product.description}
+                    </Typography>
+                    <Typography className={classes.textCenter} variant="body2" component="p">
+                        {text}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Edit description</Button>
+                </CardActions>
+            </Card>
+        </>
     )
 }
 
@@ -48,6 +66,7 @@ ProductCard.propTypes = {
         name: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
     }).isRequired,
+    text: PropTypes.string.isRequired
 }
 
 export default ProductCard;
--- eshop-rest/ui/src/pages/Products/SingleProduct.js
+++ eshop-rest/ui/src/pages/Products/SingleProduct.js
@@ -3,16 +3,24 @@ import {useEffect, useState} from "react";
 import {fetchSingleProduct} from "../../api/productsApi";
 import Loader from "../common/Loader";
 import ProductCard from "./ProductCard";
+import {getRandomText} from "../../api/loremIpsumApi";
 export default () => {
 
     const { id } = useParams();
     const [isLoading, setLoading] = useState(true);
     const [product, setProduct] = useState();
+    const [randomText, setRandomText] = useState();
 
     useEffect(() => {
-        fetchSingleProduct(id)
-            .then(r => {
-                setProduct(r.data);
+
+        Promise.all([fetchSingleProduct(id), getRandomText()])
+            .then((results) => {
+                const response1 = results[0];
+                const response2 = results[1];
+
+                setProduct(response1.data);
+                setRandomText(response2.data[0]);
+
             })
             .finally(() => {
                 setLoading(false);
@@ -23,7 +31,7 @@ export default () => {
     return (
         <>
             {isLoading && <Loader />}
-            {!isLoading && <ProductCard product={product} />}
+            {!isLoading && <ProductCard product={product} text={randomText} />}
         </>
     )
 }
