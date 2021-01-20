3228479a39aacf4bd40d9993a29e02855e99362e
Tadas Simonavičius <tadas@inventi.lt>
2021-01-20 18:41:27 +0200

    Added Redux logger middleware

--- eshop-rest/ui/src/store/index.js
+++ eshop-rest/ui/src/store/index.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cart from './slices/cartSlice'
+import {logger} from "redux-logger";
 
 export default (initialState) => configureStore({
 	reducer: {
 		cart
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
