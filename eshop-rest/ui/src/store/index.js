bccb268a94e97859a11fe6119f2f0c645616b8e4
Tadas Simonavičius <tadas@inventi.lt>
2021-01-20 20:18:55 +0200

    Added redux state serialization and deserialization from localStorage

--- eshop-rest/ui/src/store/index.js
+++ eshop-rest/ui/src/store/index.js
@@ -1,10 +1,23 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cart from './slices/cartSlice'
 import {logger} from "redux-logger";
+import {loadFromStorage, saveToStorage} from "../utils/localStorage";
 
-export default (initialState) => configureStore({
-	reducer: {
-		cart
-	},
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
-})
+export default (initialState) => {
+
+	const store = configureStore({
+		reducer: {
+			cart
+		},
+		middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+		preloadedState: loadFromStorage('state')
+	})
+
+	store.subscribe(() => {
+		const state = store.getState()
+
+		saveToStorage('state', state)
+	})
+
+	return store
+}
