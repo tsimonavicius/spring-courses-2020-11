f7706ef68c1a0050807a7e76a0ae4ae48f412db1
Tadas Simonavičius <tadas@inventi.lt>
2021-01-21 19:52:05 +0200

    Add success authentication callback implementation with JWT creation

--- eshop-rest/backend/src/main/java/com/codeacademy/backend/security/JwtAuthenticationFilter.java
+++ eshop-rest/backend/src/main/java/com/codeacademy/backend/security/JwtAuthenticationFilter.java
@@ -1,14 +1,19 @@
 package com.codeacademy.backend.security;
 
+import java.io.IOException;
 import java.util.Map;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -21,6 +26,12 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     private static final String USERNAME = "username";
     private static final String PASSWORD = "password";
+    private JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtProvider jwtProvider) {
+        super(authenticationManager);
+        this.jwtProvider = jwtProvider;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
@@ -37,5 +48,15 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         } catch (Exception e) {
             throw new BadCredentialsException("Provided structure was incorrect.");
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+
+        String jwtToken = jwtProvider.createToken(user);
+
+        response.addHeader("Authorization", "Bearer " + jwtToken);
     }
 }
