fc1e146aae32298dc57d6e004172fbabd2bf2e3b
Tadas Simonavičius <tadas@inventi.lt>
2021-01-25 20:40:32 +0200

    Added custom Authentication entry point.

--- eshop-rest/backend/src/main/java/com/codeacademy/backend/config/SecurityConfig.java
+++ eshop-rest/backend/src/main/java/com/codeacademy/backend/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.codeacademy.backend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import com.codeacademy.backend.security.JwtAuthenticationFilter;
 import com.codeacademy.backend.security.JwtAuthorizationFilter;
@@ -30,6 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .antMatchers("/login").permitAll()
                     .anyRequest().authenticated()
+                    .and()
+                .exceptionHandling()
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                     .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtProvider))
                 .addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtProvider));
