fe9ed47253fff0429e215a56ce94018e50d15d8c
Tadas Simonavičius <tadas@inventi.lt>
2021-01-26 19:07:28 +0200

    Added fields to Login page

--- eshop-rest/ui/src/pages/Login/Login.js
+++ eshop-rest/ui/src/pages/Login/Login.js
@@ -1,4 +1,23 @@
+import {Form, Formik, Field, ErrorMessage} from "formik"
 
-export default () => (
-	<h1>Login</h1>
-)
+export default () => {
+
+	return (
+		<Formik>
+			{(props) => (
+				<Form>
+					<div className="form-group">
+						<label htmlFor="username">Username:</label>
+						<Field name="username" id="username" className="form-control" placeholder="Please enter your username" />
+					</div>
+					<div className="form-group">
+						<label htmlFor="password">Password:</label>
+						<Field name="password" id="password" className="form-control" placeholder="Please enter your password" />
+					</div>
+
+					<button type="submit" className="btn btn-primary mt-2">Login</button>
+				</Form>
+			)}
+		</Formik>
+	)
+}
