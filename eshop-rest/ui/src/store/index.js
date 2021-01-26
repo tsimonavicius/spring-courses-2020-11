35138b15462cb3c8b5e86611d2528e66fcbfdf0e
Tadas Simonavičius <tadas@inventi.lt>
2021-01-26 21:28:54 +0200

    Created Redux user slice

--- eshop-rest/ui/src/store/index.js
+++ eshop-rest/ui/src/store/index.js
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cart, {loadCartFromStorage, subscribeToCartChanges} from './slices/cartSlice'
 import {logger} from "redux-logger";
+import user from './slices/userSlice'
 
 export default (initialState) => {
 
 	const store = configureStore({
 		reducer: {
-			cart
+			cart,
+			user
 		},
 		middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 		preloadedState: {cart: loadCartFromStorage(), ...initialState}
