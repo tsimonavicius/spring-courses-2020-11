33c899dde0409b2a3c59aaaf687245900887ad16
Tadas Simonavičius <tadas@inventi.lt>
2021-01-27 20:50:41 +0200

    Enabled PreAuthorize annotations

--- eshop-rest/backend/src/main/java/com/codeacademy/backend/config/SecurityConfig.java
+++ eshop-rest/backend/src/main/java/com/codeacademy/backend/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +24,7 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
  * @author tsimonavicius
  */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
